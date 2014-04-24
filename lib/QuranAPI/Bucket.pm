d42c6c184cac24ceeb4a638f7012636bb0a8ebd3
Nour Sharabash <nour.sharabash@gmail.com>
2014-04-24 16:00:12 -0500

    [#69823020] - added mp3 format to bucket output and an is_enabled flag on the audio.file table; if an mp3 (or ogg) is broken, we can disable it by toggling this flag. e.g. see /bucket/ayat/17/105?audio=11 -- there is no "mp3: {}" hash because we discovered a broken mp3 on that ayah (do not yet know why)

--- lib/QuranAPI/Bucket.pm
+++ lib/QuranAPI/Bucket.pm
@@ -277,6 +277,16 @@ sub bucket {
             my %result;
             my @result = $self->db->query( qq|
                 select a.ayah_key
+                     , ogg.url ogg_url
+                     , ogg.duration ogg_duration
+                     , ogg.mime_type ogg_mime_type
+                     , mp3.url mp3_url
+                     , mp3.duration mp3_duration
+                     , mp3.mime_type mp3_mime_type
+                  from audio.file f
+                  join quran.ayah a using ( ayah_key )
+                  left join ( select t.recitation_id
+                     , f.ayah_key
                      , concat( 'http://audio.quran.com:9999/', concat_ws( '/', r.path, s.path, f.format, concat( replace( format('%3s', a.surah_id ), ' ', '0' ), replace( format('%3s', a.ayah_num ), ' ', '0' ), '.', f.format ) ) ) url
                      , f.duration
                      , f.mime_type
@@ -285,13 +295,39 @@ sub bucket {
                   join audio.recitation t using ( recitation_id )
                   join audio.reciter r using ( reciter_id )
                   left join audio.style s using ( style_id )
-                 where t.recitation_id = ?
+                 where f.is_enabled and f.format = 'ogg' ) ogg using ( ayah_key, recitation_id )
+                  left join ( select t.recitation_id
+                     , f.ayah_key
+                     , concat( 'http://audio.quran.com:9999/', concat_ws( '/', r.path, s.path, f.format, concat( replace( format('%3s', a.surah_id ), ' ', '0' ), replace( format('%3s', a.ayah_num ), ' ', '0' ), '.', f.format ) ) ) url
+                     , f.duration
+                     , f.mime_type
+                  from audio.file f
+                  join quran.ayah a using ( ayah_key )
+                  join audio.recitation t using ( recitation_id )
+                  join audio.reciter r using ( reciter_id )
+                  left join audio.style s using ( style_id )
+                 where f.is_enabled and f.format = 'mp3' ) mp3 using ( ayah_key, recitation_id )
+                 where f.recitation_id = ?
                    and a.ayah_key in ( |.( join ', ', map { '?' } @keys ).qq| )
-                   and f.format = 'ogg'
+                 group by a.ayah_key, ogg.url, ogg.duration, ogg.mime_type, mp3.url, mp3.duration, mp3.mime_type
                  order by a.surah_id, a.ayah_num
             |, $vars{audio}, @keys )->hashes;
             for my $result ( @result ) {
                 my $ayah_key = delete $result->{ayah_key};
+                my %mash;
+                   $mash{ogg} = [ qw/url duration mime_type/ ];
+                   $mash{mp3} = [ qw/url duration mime_type/ ];
+                for my $pfix ( keys %mash ) {
+                    my ( @grep, %hash );
+                    @grep = grep { $_ =~ /^${pfix}_/ } keys %{ $result };
+                    for my $orig ( @grep ) {
+                        my $attr = $orig;
+                           $attr =~ s/^${pfix}_//;
+                        $hash{ $attr } = delete $result->{ $orig };
+                    }
+                    delete $hash{ $_ } for grep { not defined $hash{ $_ } } keys %hash;
+                    $result->{ $pfix } = \%hash if keys %hash;
+                }
                 $result{ $ayah_key } = $result;
             }
             return \%result;
--- t/002_basic.t
+++ t/002_basic.t
@@ -54,13 +54,4 @@ $t->get_ok( '/options/default' )
 $t->get_ok( '/' )
     ->status_is( 302 );
 
-$t->get_ok( '/docs' )
-    ->status_is( 200 );
-
-$t->get_ok( '/docs/options/content' )
-    ->status_is( 200 );
-
-$t->get_ok( '/docs/QuranAPI::Options::Content' )
-    ->status_is( 200 );
-
 done_testing;
